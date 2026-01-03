cfed06b5e76663f0144031835db04f9928a7e4ef
sivakumar reddy <info@joindevops.com>
2026-01-03 09:17:52 +0530

    jenkins

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -39,6 +39,7 @@ def call (Map configMap){
                                 kubectl get nodes
                                 sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
                                 helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m .
+                                #kubectl apply -f ${COMPONENT}-${deploy_to}.yaml
                             """
                         }
                     }
